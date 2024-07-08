6ee633b7596d775fe627d63f436a42e0535f7ad6
nombre-usuario2 <correo-usuario2>
2024-07-08 18:23:21 -0400

    refactor con una opcion de uso de componente

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,4 @@
-import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components/ProductCard'
+import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components'
 
 const product = {
     id: '1',
@@ -15,11 +15,11 @@ export const ShoppingPage = () => {
             flexDirection: 'row',
             flexWrap: 'wrap'
         }}>
-            <ProductCard product={product}>
+            {/* <ProductCard product={product}>
               <ProductCard.Image />
               <ProductCard.Title title='Café' />
               <ProductCard.Buttons />
-            </ProductCard>
+            </ProductCard> */}
             <ProductCard product={product}>
               <ProductImage />
               <ProductTitle />
