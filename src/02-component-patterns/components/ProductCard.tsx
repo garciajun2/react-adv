bfec4a2dec71753d51896ad14d065c4e6af29202
nombre-usuario2 <correo-usuario2>
2024-07-11 09:29:00 -0400

    custom styles listo

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,6 +1,6 @@
 import styles from '../styles/styles.module.css';
 import { useProduct } from '../hooks/useProduct';
-import { createContext, ReactElement } from 'react';
+import { createContext, CSSProperties, ReactElement } from 'react';
 import { Product, ProductContextProps } from '../interfaces/interfaces';
 
 export const ProductContext = createContext({} as ProductContextProps)
@@ -9,10 +9,11 @@ const { Provider } = ProductContext;
 export interface Props {
   product: Product,
   children?: ReactElement | ReactElement[],
-  className?: string
+  className?: string,
+  style?: CSSProperties
 }
 
-export const ProductCard = ({ children, product, className }: Props) => {
+export const ProductCard = ({ children, product, className, style }: Props) => {
     const { counter, increaseBy  } = useProduct();
     
   return (
@@ -21,7 +22,10 @@ export const ProductCard = ({ children, product, className }: Props) => {
         increaseBy, 
         product
     }}>
-        <div className={ `${ styles.productCard } ${ className }` }>
+        <div 
+          className={ `${ styles.productCard } ${ className }` }
+          style={ style }
+        >
             {children}
         </div>
     </Provider>
--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -1,14 +1,15 @@
-import { useContext } from "react";
+import { CSSProperties, useContext } from "react";
 import { ProductContext } from "./ProductCard";
 import noImage from '../assets/no-image.jpg';
 import styles from '../styles/styles.module.css';
 
 export interface Props {
     img?: string,
-    className?: string
+    className?: string,
+    style?: CSSProperties
 }
 
-export const ProductImage = ({ img, className }: Props) => {
+export const ProductImage = ({ img, className, style }: Props) => {
     const { product } = useContext(ProductContext);
     let imgToShow: string;
 
@@ -20,6 +21,11 @@ export const ProductImage = ({ img, className }: Props) => {
         imgToShow = noImage;
 
     return (
-        <img className={ `${ styles.productImg } ${ className }` } src={ imgToShow } alt='Product'/>
+        <img 
+            className={ `${ styles.productImg } ${ className }` } 
+            src={ imgToShow } 
+            alt='Product'
+            style={ style }
+        />
     )
 }
