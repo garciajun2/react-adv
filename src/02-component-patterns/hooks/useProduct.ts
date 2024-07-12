988af142b9b85d79639d3f1d850e6fe9ec255036
nombre-usuario2 <correo-usuario2>
2024-07-12 14:23:59 -0400

    alternativo no tan bien

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from "../interfaces/interfaces";
 
 interface useProductArgs {
@@ -9,11 +9,7 @@ interface useProductArgs {
 
 export const useProduct = ( { onChange, product, value = 0 }:useProductArgs ) => {
     const [counter, setcounter] = useState(value);
-    const isControlled = useRef(!!onChange);
     const increaseBy = (value: number) => {
-        if(isControlled.current) {
-            return onChange!({count: value, product});
-        }
         const newValue = Math.max(counter + value, 0);
         setcounter(newValue);
         onChange && onChange({ count: newValue, product });
