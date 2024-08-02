aab8e292e017d010914e9d5362a4af7eb40c1df3
nombre-usuario2 <correo-usuario2>
2024-08-02 11:12:33 -0400

    Cambios para publicar npm

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,5 +1,4 @@
 import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components';
-import '../styles/custom-styles.css';
 import { products } from '../data/products';
 
 const product = products[0];
@@ -12,7 +11,6 @@ export const ShoppingPage = () => {
         <hr/>
 
         <ProductCard
-          className="bg-dark text-white"
           key={ product.id }
           product={ product }
           initialValues={{
@@ -23,15 +21,9 @@ export const ShoppingPage = () => {
           {
             ({ reset, isMaxCountReached, maxCount, count, increaseBy }) => (
               <>
-                <ProductImage className="custom-image" />
-                <ProductTitle className="text-bold" />
-                <ProductButtons className="custom-buttons" />
-                <button onClick={ reset }>Reset</button>
-                <button onClick={() => increaseBy(-2)}>-2</button>
-                {
-                  ( !isMaxCountReached && <button onClick={() => increaseBy(+2)}>+2</button> )
-                }
-                <span>{count} - {maxCount}</span>
+                <ProductImage />
+                <ProductTitle />
+                <ProductButtons />
               </>
             )
           }
